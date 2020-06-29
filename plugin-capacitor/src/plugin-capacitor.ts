6f637fb695a96913f84348ca757c6d1d5e332982
wuhongmei <1113538198@qq.com>
2020-06-29 23:57:42 +0800

    update plugin

--- plugin-capacitor/src/plugin-capacitor.ts
+++ plugin-capacitor/src/plugin-capacitor.ts
@@ -1,7 +1,7 @@
 import address from 'address';
 import { resolve } from 'path';
 import { IApi } from 'umi';
-import { readFileJson, writeFileJson } from './utils';
+import { readFileJson, writeFileJson, spawn } from './utils';
 import { run as runAndroid } from './build-android';
 
 async function changeCapacitorConfig(configPath: string, url: string): Promise<void> {
@@ -59,13 +59,31 @@ export default (api: IApi) => {
       first = false;
       const url = `http://${address.ip()}:${api.getPort()}`;
       logger.info(`Setting url(${url}) to server.url in capacitor.config.json`);
-      await changeCapacitorConfig(resolve(<string>api.paths.cwd, 'capacitor.config.json'), url).catch(() => undefined);
+      await changeCapacitorConfig(resolve(<string>api.paths.cwd, 'capacitor.config.json'), url);
       // const { capacitor = {} } = api.config;
       if (android) {
         runAndroid();
       } else if (ios) {
-        // runIos();
+        // TODO: runIos();
+        await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['copy', 'ios']);
+        await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['open', 'ios']);
       }
+    }
+  });
+
+  api.onBuildComplete(async () => {
+    const { android, ios } = api.args;
+    if (!android && !ios) {
+      return;
+    }
+    await changeCapacitorConfig(resolve(<string>api.paths.cwd, 'capacitor.config.json'), 'index.html');
+    // const { capacitor = {} } = api.config;
+    if (android) {
+      await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['copy', 'android']);
+      await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['open', 'android']);
+    } else if (ios) {
+      await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['copy', 'ios']);
+      await spawn(resolve(process.cwd(), 'node_modules/.bin/cap'), ['open', 'ios']);
     }
   });
 };
